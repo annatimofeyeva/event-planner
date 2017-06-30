a0ef32741ee022f9f75405a6c552af2eeb880109
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-06-30 14:44:35 -0700

    declare Event class and write Constructor method

---
+++ src/main/java/Event.java
@@ -0,0 +1,17 @@
+public class Event{
+  private int mGuestsNumber;
+  private String mFoodOption;
+  private String mBeveragesOption;
+  private String mEntertainmentOption;
+
+public Event(int gnumber, String food, String beverages, String entertainment){
+  mGuestsNumber = gnumber;
+  mFoodOption = food;
+  mBeveragesOption = beverages;
+  mEntertainmentOption = entertainment;
+  }
+
+
+
+
+}
